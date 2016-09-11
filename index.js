5d9a3343f273139078959c2f04f11f83e6a3ceb8
aniruddha <achakraborty510@gmail.com>
2016-09-11 12:58:52 +0600

    added connections

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var request = require('superagent');
 
 class index {
@@ -5,13 +7,27 @@ class index {
 		constructor (connection) {
 
 			if (typeof connection !== 'object') {
-				throw new Error('Connection value must be passed by an object');
+
+				throw new Error('Connection parameters must be passed by an object');
 			}
 
-			this.host = connection.host;
-			this.username = connection.username;
-			this.password = connection.password;
-			this.port = connection.port || 7474;
+/*
+			this.http     = connection.http || 'http://';
+			this.host 	  = connection.host || 'locahost';
+			this.username = connection.username || 'neo4j';
+			this.password = connection.password || '123456';
+			this.port 	  = connection.port || '7474';
+			this.url 	  = this.http+this.username+':'+this.password+'@'+this.host+':'+this.port;
+
+			this.dbLink = this.url.'/db/data'
+ 
+			request.get(this.dbLink)
+				   .set('Accept', 'application/json')
+				   .set('X-Stream','true');
+ */
 
 		}
+
 }
+
+module.exports = index;
---
+++ test.js
@@ -0,0 +1,3 @@
+var neo4j = require('./index');
+
+var db = new neo4j([]);
